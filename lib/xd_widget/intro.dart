66c15f47415d1e6c53e2189fd41333f249f1fbca
moussakareGueye <m.gueye@amdistributori.it>
2022-03-16 17:25:06 +0100

    intro.dart

--- lib/xd_widget/intro.dart
+++ lib/xd_widget/intro.dart
@@ -4,6 +4,7 @@ class Intro extends StatelessWidget {
   Intro({
     Key? key,
   }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,74 +12,79 @@ class Intro extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           Container(
-              width: 657.5,
-              height: 820.5,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: const AssetImage('assets/images/background_intro.jpg'),
-                  fit: BoxFit.fill,
-                  colorFilter: new ColorFilter.mode(
-                      Colors.black.withOpacity(0.3), BlendMode.dstIn),
+            width: 657.5,
+            height: 820.5,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: const AssetImage('assets/images/background_intro.jpg'),
+                fit: BoxFit.fill,
+                colorFilter: new ColorFilter.mode(
+                    Colors.black.withOpacity(0.3), BlendMode.dstIn),
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'iDrobe',
+                  style: TextStyle(
+                    fontFamily: '.AppleSystemUIFont',
+                    fontSize: 60,
+                    color: const Color(0xff5c5c5c),
+                  ),
+                  textAlign: TextAlign.left,
                 ),
               ),
-            ),
-          Container(),
-          Container(),
-          Transform.translate(
-            offset: Offset(104.0, 346.0),
-            child: Text(
-              'iDrobe',
-              style: TextStyle(
-                fontFamily: '.AppleSystemUIFont',
-                fontSize: 60,
-                color: const Color(0xff5c5c5c),
+              SizedBox(
+                height: 20,
               ),
-              textAlign: TextAlign.left,
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(74.0, 412.0),
-            child: Text(
-              'my wardrobe in one device',
-              style: TextStyle(
-                fontFamily: '.AppleSystemUIFont',
-                fontSize: 20,
-                color: const Color(0xff5c5c5c),
-                fontWeight: FontWeight.w100,
-              ),
-              textAlign: TextAlign.left,
-            ),
-          ),
-          Transform.translate(
-            offset: Offset(137.0, 696.0),
-            child: Container(
-              width: 102.0,
-              height: 43.0,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: const AssetImage('assets/images/logo_zara.png'),
-                  fit: BoxFit.fill,
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'my wardrobe in one device',
+                  style: TextStyle(
+                    fontFamily: '.AppleSystemUIFont',
+                    fontSize: 20,
+                    color: const Color(0xff5c5c5c),
+                    fontWeight: FontWeight.w100,
+                  ),
+                  textAlign: TextAlign.left,
                 ),
               ),
-            ),
+            ],
           ),
-          Transform.translate(
-            offset: Offset(111.0, 667.0),
-            child:
-                // Adobe XD layer: 'Focus on what matter' (text)
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'In collaborazione con',
+                  style: TextStyle(
+                    fontFamily: 'Roboto-Regular',
+                    fontSize: 16,
+                    color: const Color(0xff7b7890),
+                    height: 1.5,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                 Container(
+                  width: 102.0,
+                  height: 43.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: const AssetImage('assets/images/logo_zara.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
                 SizedBox(
-              width: 153.0,
-              height: 22.0,
-              child: Text(
-                'In collaborazione con',
-                style: TextStyle(
-                  fontFamily: 'Roboto-Regular',
-                  fontSize: 16,
-                  color: const Color(0xff7b7890),
-                  height: 1.5,
-                ),
-                textAlign: TextAlign.left,
+                height: 50,
               ),
+              ],
             ),
           ),
         ],
